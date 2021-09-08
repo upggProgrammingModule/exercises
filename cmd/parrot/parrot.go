d44845901fae38f9dfc5082dc8baf747fb49cb24
RP Pornmongkolsuk <82060975+RP-Pornmongkolsuk@users.noreply.github.com>
2021-09-08 14:42:35 -0400

    fixed parrot bugs

--- cmd/parrot/parrot.go
+++ cmd/parrot/parrot.go
@@ -14,7 +14,7 @@ import (
 // text as the input string, but with all letters
 // capitalized if shout is true.
 func adjustVolume(word string, shout bool) string {
-	var adjustedText string
+	var adjustedText string = word
 	if shout {
 		adjustedText = strings.ToUpper(word)
 	}
@@ -36,7 +36,7 @@ func repeatWord(word string, repetitions int) string {
 	// we are now dealing with the case that we must
 	// repeat the word one, or more, times
 	response = word
-	for i = 1; i <= repetitions; i++ {
+	for i = 2; i <= repetitions; i++ {
 		response = fmt.Sprintf("%s %s", response, word)
 	}
 
