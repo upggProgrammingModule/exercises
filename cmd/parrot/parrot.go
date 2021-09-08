3a1cec6c4cc442638bcfe3b672d3ad5b1c2832fb
Angela-Jones <angelagracejones@gmail.com>
2021-09-08 14:36:44 -0400

    Fixed 2 erros in parrot.go

--- cmd/parrot/parrot.go
+++ cmd/parrot/parrot.go
@@ -15,6 +15,7 @@ import (
 // capitalized if shout is true.
 func adjustVolume(word string, shout bool) string {
 	var adjustedText string
+	adjustedText = word
 	if shout {
 		adjustedText = strings.ToUpper(word)
 	}
@@ -36,7 +37,7 @@ func repeatWord(word string, repetitions int) string {
 	// we are now dealing with the case that we must
 	// repeat the word one, or more, times
 	response = word
-	for i = 1; i <= repetitions; i++ {
+	for i = 2; i <= repetitions; i++ {
 		response = fmt.Sprintf("%s %s", response, word)
 	}
 
