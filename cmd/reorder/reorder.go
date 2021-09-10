79b25c713a64b56c30bd261ad1cf280630b0d8d6
Latanya <90213950+Latanyacoke@users.noreply.github.com>
2021-09-10 15:35:44 -0400

    Latanyareorder

--- cmd/reorder/reorder.go
+++ cmd/reorder/reorder.go
@@ -1,17 +1,27 @@
-package main
-
+package main 
 import (
 	"flag"
 	"fmt"
 	"github.com/vertgenlab/gonomics/fileio"
 	"log"
+	"strings"
+	"strconv"
 )
 
 // commaListToInts will take in a string of comma
 // separated numbers and return a slice of ints
 // representing the numbers in the input string
 func commaListToInts(commaList string) []int {
-	return []int{1, 4, 3}
+        
+        commaSlice := strings.Split(commaList, ",")
+        var new_list []int
+        new_list = make ([]int, len(commaSlice))
+        var i int
+        for i=0; i < len (commaSlice); i++ {
+        n,_ := strconv.Atoi (commaSlice [i])
+        new_list[i] = n
+        }
+        return new_list
 }
 
 // oneBasedToZeroBased will take a list of numbers in 1-based
@@ -22,24 +32,44 @@ func oneBasedToZeroBased(inputNumbers []int) []int {
 
 	// loop through the list of input numbers and
 	// build a new list by subtracting one from each.
-	return []int{0, 3, 2}
+	var i int
+        for i = 0; i <len(inputNumbers); i++ {
+	inputNumbers[i] = inputNumbers [i] - 1
+        }
+
+return inputNumbers
+	
 }
 
 // reorderColumns will take in the line of a file, split it into columns
 // based on delimiter, and then put a subset of the columns back into
 // a string based on the order of their indices in columnOrder
 // columnOrder is zero-based with the first field having an index of zero
-func reorderColumns(line string, delimiter string, columnOrder []int) string {
 	// TODO: write this function
 
-	//split the string based on the delimiter
+func reorderColumns(line string, delimiter string, columnOrder []int) string {
+        // TODO: write this function
 
-	//build a new string while looping through columnOrder
+        //split the string based on the delimiter
+	lineSlice := strings.Split(line, delimiter)
 
-	//return the new string you built
-	return "happy"
+
+        //build a new string while looping through columnOrder
+
+	var i int
+	var new_line string
+	
+	for i=0; i < len (columnOrder); i++ {
+	
+	new_line = new_line + lineSlice[columnOrder[i]]
+	
+	}
+	
+
+        //return the new string you built
+
+        return new_line
 }
-
 func reorder(inputFilename string, columnDelimiter string, commaListOfFields string, outputFilename string) {
 	var inFile *fileio.EasyReader = fileio.EasyOpen(inputFilename)
 	var outFile *fileio.EasyWriter = fileio.EasyCreate(outputFilename)
@@ -80,6 +110,7 @@ func usage() {
 }
 
 func main() {
+	fmt.Printf("%v\n", commaListToInts("1,7,9"))
 	var expectedNumArgs int = 3
 	var delim *string = flag.String("delimiter", "\t", "The string that delimits columns in the input")
 
@@ -96,3 +127,4 @@ func main() {
 
 	reorder(inputFilename, *delim, listOfFields, outputFilename)
 }
+
